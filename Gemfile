840db076abb5e50cfbf4c9f75bebc90cc93213ed
jiayue zhu <1638790221@qq.com>
2018-07-27 10:16:09 +0800

    I did the basic backend

--- Gemfile
+++ Gemfile
@@ -3,6 +3,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.4.4'
 
+# added gems
+gem 'autoprefixer-rails'
+gem 'bootstrap-sass', '~> 3.3.7'
+gem 'font-awesome-sass', '~> 5.0.9'
+gem 'simple_form'
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+require 'faker'
+
+Cocktail.delete_all
+puts "old cocktails deleted"
+
+Dose.delete_all
+puts "old doses deleted"
+
+Ingredient.delete_all
+puts "old ingredients deleted"
+
+a = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+ingredient = JSON.parse(a)
+
+15.times do
+  Cocktail.create(name: Faker::Food.dish)
+end
+puts "5 cocktails created !"
+
+50.times do
+  Ingredient.create(name: ingredient["drinks"].sample["strIngredient1"])
+end
+puts "50 ingredients generated !"
+
+Cocktail.all.each do |c|
+  a = rand(1..5)
+  a.times do
+   Dose.create(description: Faker::Food.description,
+               cocktail_id: c.id,
+               ingredient_id: Ingredient.all.sample.id)
+  end
+end
+puts "cocktail, dose and ingredients linked !"
+puts "finished seeding !"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
